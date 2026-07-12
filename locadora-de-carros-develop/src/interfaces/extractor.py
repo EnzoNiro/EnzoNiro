eb31a5e7d647907f9a8349d91f589d4cb533d9b8
Enzo Lauricella Niro <EnzoNiro@users.noreply.github.com>
2026-07-12 16:20:14 -0300

    Delete locadora-de-carros-develop directory

--- locadora-de-carros-develop/src/interfaces/extractor.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from pyspark.sql import DataFrame
-
-class ExtractorInterface(ABC):
-    @abstractmethod
-    def extract(self) -> DataFrame:
-        """Extrai os dados e retorna como um DataFrame do PySpark."""
-        pass
--- locadora-de-carros-develop/src/interfaces/loader.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from pyspark.sql import DataFrame
-from typing import Dict
-
-class LoaderInterface(ABC):
-    @abstractmethod
-    def load(self, dataframes: Dict[str, DataFrame]) -> None:
-        """Carrega (salva) os dataframes nos destinos finais."""
-        pass
